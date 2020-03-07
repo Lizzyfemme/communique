c2aee5a426a0e24ee02ffdc4bc15c9bd2436e02c
Lizzyfemme <lizzy_swann@hotmail.com>
2020-03-06 17:24:02 -0800

    Post coming from the backend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { MatCardModule } from "@angular/material/card";
 import { MatButtonModule } from "@angular/material/button";
 import { MatToolbarModule } from "@angular/material/toolbar";
 import { MatExpansionModule } from "@angular/material/expansion";
+import { HttpClientModule } from "@angular/common/http";
 
 import { AppComponent } from "./app.component";
 import { PostCreateComponent } from "./posts/post-create/post-create.component";
@@ -23,6 +24,7 @@ import { PostListComponent } from "./posts/post-list/post-list.component";
   imports: [
     BrowserModule,
     FormsModule,
+    HttpClientModule,
     BrowserAnimationsModule,
     MatInputModule,
     MatCardModule,
