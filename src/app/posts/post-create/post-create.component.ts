1b92baa41373fea0e058117eb5038dbcc245c226
Lizzyfemme <lizzy_swann@hotmail.com>
2020-03-11 06:21:34 -0700

    Edit button goes to edit route

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,7 +1,8 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { NgForm } from "@angular/forms";
-
+import { Post } from "../post.model";
 import { PostsService } from "../posts.service";
+import { ActivatedRoute, ParamMap } from "@angular/router";
 
 @Component({
   selector: "app-post-create",
@@ -11,8 +12,26 @@ import { PostsService } from "../posts.service";
 export class PostCreateComponent {
   enteredTitle = "";
   enteredContent = "";
+  post: Post;
+  private mode = "create";
+  private postId: string;
 
-  constructor(public postsService: PostsService) {}
+  constructor(
+    public postsService: PostsService,
+    public route: ActivatedRoute
+  ) {}
+  ngOnInit() {
+    this.route.paramMap.subscribe((paramMap: ParamMap) => {
+      if (paramMap.has("postId")) {
+        this.mode = "edit";
+        this.postId = paramMap.get("postId");
+        this.post = this.postsService.getPost(this.postId);
+      } else {
+        this.mode = "create";
+        this.postId = null;
+      }
+    });
+  }
 
   onAddPost(form: NgForm) {
     if (form.invalid) {
