15489712f52757a3ca83ebeee30254be4c4959ba
Lizzyfemme <lizzy_swann@hotmail.com>
2020-03-11 19:18:06 -0700

    edit routes returns content if reloaded

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -35,7 +35,9 @@ export class PostsService {
     return this.postsUpdated.asObservable();
   }
   getPost(id: string) {
-    return { ...this.posts.find(p => p.id === id) };
+    return this.http.get<{ _id: string; title: string; content: string }>(
+      "http://localhost:3000/api/posts/" + id
+    );
   }
   addPost(title: string, content: string) {
     const post: Post = { id: null, title: title, content: content };
@@ -55,7 +57,13 @@ export class PostsService {
     const post: Post = { id: id, title: title, content: content };
     this.http
       .put("http://localhost:3000/api/posts/" + id, post)
-      .subscribe(response => console.log(response));
+      .subscribe(response => {
+        const updatedPosts = [...this.posts];
+        const oldPostIndex = updatedPosts.findIndex(p => p.id === post.id);
+        updatedPosts[oldPostIndex] = post;
+        this.posts = updatedPosts;
+        this.postsUpdated.next([...this.posts]);
+      });
   }
   deletePost(postId: string) {
     this.http
