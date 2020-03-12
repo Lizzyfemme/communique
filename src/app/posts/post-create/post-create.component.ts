8a1ae04593c599c5bcd47be94119a66299ebf9f7
Lizzyfemme <lizzy_swann@hotmail.com>
2020-03-12 10:59:31 -0700

    image preview added

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -15,6 +15,7 @@ export class PostCreateComponent {
   post: Post;
   isLoading = false;
   form: FormGroup;
+  imagePreview: string;
   private mode = "create";
   private postId: string;
 
@@ -29,6 +30,9 @@ export class PostCreateComponent {
       }),
       content: new FormControl(null, {
         validators: [Validators.required, Validators.minLength(3)]
+      }),
+      image: new FormControl(null, {
+        validators: [Validators.required]
       })
     });
     this.route.paramMap.subscribe((paramMap: ParamMap) => {
@@ -53,6 +57,16 @@ export class PostCreateComponent {
         this.postId = null;
       }
     });
+  }
+  onImagePicked(event: Event) {
+    const file = (event.target as HTMLInputElement).files[0];
+    this.form.patchValue({ image: file });
+    this.form.get("image").updateValueAndValidity();
+    const reader = new FileReader();
+    reader.onload = () => {
+      this.imagePreview = reader.result as string;
+    };
+    reader.readAsDataURL(file);
   }
 
   onSavePost() {
